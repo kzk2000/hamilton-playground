403dd054646f24812f9641ff35b2331cd735c83d
kzk2000 <kilianm@gmail.com>
2023-11-19 15:26:19 -0500

    added error msg

--- toy_nodes.py
+++ toy_nodes.py
@@ -1,13 +1,13 @@
 from hamilton.htypes import Parallelizable, Collect
 
 
-def url() -> Parallelizable[str]:
+def urls() -> Parallelizable[str]:
     for url_ in ['url_a', 'url_b']:
         yield url_
 
 
-def counts(url: str) -> int:
-    return len(url_loaded.split("_"))
+def counts(urls: str) -> int:
+    return len(urls.split("_"))
 
 
 def total_words(counts: Collect[int]) -> int:
--- toy_run.py
+++ toy_run.py
@@ -20,7 +20,11 @@ dr = (
 
 output_columns = [
     'total_words',
-    'total_words2',  # having both enabled leads to error
+    'total_words2',
+    # having both 'total_words' and 'total_word2' enabled leads to this error
+    #   File "/home/user/Programs/anaconda3/envs/py310/lib/python3.10/site-packages/hamilton/execution/state.py", line 113, in read
+    #     raise KeyError(f"Key {formatted_key} not found in cache")
+    # KeyError: 'Key total_words2 not found in cache'
 ]
 
 
